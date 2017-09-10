a143280034d61416ed3acb8ec6af153300195a6b
mbasso <mbasso96@gmail.com>
2017-09-10 11:23:31 +0200

    webpack config

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+exports.__esModule = true;
+
+var _language = require('./language');
+
+exports['default'] = _language.parser;
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,55 @@
-'use strict';
-
 var webpack = require('webpack');
+var resolve = require('path').resolve;
+var CompressionPlugin = require('compression-webpack-plugin');
 
-var env = process.env.NODE_ENV;
-var config = {
-  module: {
-    loaders: [
-      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
-    ],
-  },
-  output: {
-    library: 'NaturalRegex',
-    libraryTarget: 'umd',
-  },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env),
-    }),
-  ],
+module.exports = (env) => {
+  env = env || {};
+  var addPlugin = (add, plugin) => add ? plugin : undefined;
+  var ifProd = plugin => addPlugin(env.prod, plugin);
+  var removeEmpty = array => array.filter(i => !!i);
+  return {
+    entry: './index.js',
+    target: 'node',
+    output: {
+      library: 'gccx',
+      libraryTarget: 'umd',
+    },
+    context: resolve(__dirname, 'src'),
+    devtool: env.prod ? 'source-map' : 'eval',
+    bail: !!env.prod,
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: [/node_modules/, /language.js/],
+      }],
+    },
+    plugins: removeEmpty([
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      ifProd(new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false,
+        quiet: true,
+      })),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(ifProd('production') || 'development'),
+      }),
+      ifProd(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false,
+          screw_ie8: true,
+        },
+      })),
+      ifProd(new CompressionPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      })),
+    ]),
+  };
 };
-
-if (env === 'production') {
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false,
-      },
-    })
-  );
-}
-
-module.exports = config;
