9d475fdcc23db872e85997ffe1e36869d511a00b
mbasso <mbasso96@gmail.com>
2017-10-24 20:18:04 +0200

    fix help

--- src/cli/index.js
+++ src/cli/index.js
@@ -13,13 +13,13 @@ let input;
 program
   .version(version)
   .arguments('<file_or_directory>')
-  .option('-o, --output <directory>', 'Destination folder for compiled files')
-  .option('-x, --extensions <extensions>', 'List of extensions to hook into', val =>
+  .option('-o, --output <file_or_directory>', 'destination file or folder for compiled files')
+  .option('-x, --extensions <extensions>', 'list of extensions to hook into', val =>
     val.split(',').map(x => String(x).replace(/(?:(^")|("$))/g, '')))
-  .option('-i, --ignore <regex>', 'Ignore all files and directories that match this regex')
-  .option('--no-gccxrc', 'Whether or not to look up .gccxrc')
-  .option('--no-copy-files', 'When compiling a directory avoid copy over non-compilable files')
-  .option('-w, --watch', 'Compile files every time that you change them')
+  .option('-i, --ignore <regex>', 'ignore all files and directories that match this regex')
+  .option('--no-gccxrc', 'whether or not to look up .gccxrc')
+  .option('--no-copy-files', 'when compiling a directory avoid copy over non-compilable files')
+  .option('-w, --watch', 'compile files every time that you change them')
   .action((fileOrDirectory) => {
     input = fileOrDirectory;
   })
